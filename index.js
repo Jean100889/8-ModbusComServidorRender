939991363d7bc5519212867862555f448e0df23f
Jean100889 <engmec.jeansantos@gmail.com>
2024-03-26 21:21:24 -0300

    inicio

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ app.use(cors());
 
 app.post("/", jsonParser, function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
-  bit = req.body.l1;
+  cima = req.body.up;
+  baixo = req.body.down;
+  esquerda = req.body.left;
+  direita = req.body.right;
+  garra = req.body.garra;
+
   console.log(bit);
   res.end();
 });
@@ -18,7 +23,11 @@ app.get("/", function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
   res.write(
     JSON.stringify({
-      l1: bit,
+      cima: bit,
+      baixo: bit,
+      esquerda: bit,
+      direita: bit,
+      garra: bit,
     }),
   );
   res.end();
