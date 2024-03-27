e44f5355fd59da7278f86bf4d10422ddcbbe5e46
Jean100889 <engmec.jeansantos@gmail.com>
2024-03-26 21:04:52 -0300

    inicio

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-// npm i express
-const express = require("express"); //importa a framework
+const express = require("express");
 var bodyParser = require("body-parser");
-const app = express(); //atribui o nome da framework ao nome app
+const app = express();
 var jsonParser = bodyParser.json();
-// npm i cors
 var cors = require("cors");
+var bit = 0;
+
 app.use(cors());
 
-var up = 0;
-var down = 0;
-var left = 0;
-var right = 0;
-var garra = 0;
-
-//usa o express para monitorar a porta 8080 e capturar os requests feitos pelo POST
 app.post("/", jsonParser, function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
-
-  //armazena o valor recebido pela rede na variável bit
-  up = req.body.up;
-  down = req.body.down;
-  left = req.body.left;
-  right = req.body.right;
-  garra = req.body.garra;
-
+  bit = req.body.l1;
+  console.log(bit);
   res.end();
 });
 
-app.get("/", jsonParser, function (req, res) {
+app.get("/", function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
   res.write(
     JSON.stringify({
-      up: up,
-      down: down,
-      left: left,
-      right: right,
-      garra: garra,
+      l1: bit,
     }),
   );
   res.end();
